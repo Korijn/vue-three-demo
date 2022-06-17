b0cf125ab8eb13a15e887c8e89ab92f914d26297
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2022-06-17 18:12:38 +0200

    progress

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import * as THREE from 'three';
+
+import { ref } from 'vue'
+
+const cube = ref(null);
+
+const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+// const animate = () => {
+// 	requestAnimationFrame(animate);
+
+//   cube.value.rotation.x += 0.1;
+//   cube.value.rotation.y += 0.1;
+// };
+// requestAnimationFrame(animate);
+
+// ref does not work because of some weird thing
+
+</script>
+
+<template>
+  <mesh :geometry="geometry" :material="material" ref="cube" />
+</template>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import * as THREE from 'three';
 
 import { createApp } from '@korijn/vue-three'
 
-const app = createApp();
+import App from './App.vue';
+
+const app = createApp(App);
 
 const canvas = document.getElementById("app");
 
@@ -12,11 +14,26 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer({canvas});
 renderer.setSize( window.innerWidth, window.innerHeight );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+const ambientLight = new THREE.AmbientLight( 0xffffff, 0.15 );
+scene.add( ambientLight );
+
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+directionalLight.position.set(-1, 1, 1);
+scene.add( directionalLight );
 
 camera.position.z = 5;
 
+const animate = () => {
+	requestAnimationFrame(animate);
+
+	renderer.render(scene, camera);
+};
+requestAnimationFrame(animate);
+
 app.mount(scene);
+
